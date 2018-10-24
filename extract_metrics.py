3b666b90253cbe103814e5dae199064935a3f5e7
Lucas Selani de Melo <lucasselani@inatel.br>
2018-10-24 15:10:32 -0300

    changing dump to json
    Signed-off-by: Lucas Selani de Melo <lucasselani@inatel.br>

--- extract_metrics.py
+++ extract_metrics.py
@@ -12,12 +12,9 @@ import datetime
 
 result = result_model.Result()
 
-def print_results(name, data, metric):
-    metric.mean, metric.inf_mean, metric.sup_mean, metric.std\
-        = mean_confidence_interval(data)
-    return ('%s metric:\nmean=%.5f  inf_mean=%.5f  sup_mean=%.5f  std=%.5f\n') \
-        % (name, metric.mean, metric.inf_mean, metric.sup_mean, metric.std)
-
+def get_metric_results(data, metric):
+    metric['mean'], metric['inf_mean'], metric['sup_mean'], metric['std'] = mean_confidence_interval(data)
+    return metric
 
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0 * np.array(data)
@@ -91,36 +88,20 @@ def metrics(data_list, type_of_rendering, number_of_interactions):
         else:
             number_of_none_results = number_of_none_results + 1     
 
-    path = os.path.dirname(os.path.abspath(__file__)) + \
-        (('\\results\\%s_report_%s.txt') % (type_of_rendering, int(time.time())))
     result.report = type_of_rendering
     result.date = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
     result.number_of_interactions = len(data_list)
     result.total_number_of_interactions = number_of_interactions
     result.number_of_none_values = number_of_none_results
-    with open(path, 'w') as f:
-        print >> f, 'Report: %s' % type_of_rendering
-        print >> f, 'Date: %s' % datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
-        print >> f, 'Number of interactions: %s/%s' % (len(data_list), number_of_interactions)
-        print >> f, 'Number of None values: %s\n' % number_of_none_results
-        print >> f, print_results('First contentful paint', \
-            first_contentful_paint_list, result.first_contentful_paint)
-        print >> f, print_results('First meaningful paint', \
-            first_meaningful_paint_list, result.first_meaningful_paint)
-        print >> f, print_results('Speed index', \
-            speed_index_list, result.speed_index)
-        print >> f, print_results('Time to first byte', \
-            time_to_first_byte_list, result.time_to_first_byte)
-        print >> f, print_results('First CPU idle', \
-            first_cpu_idle_list, result.first_cpu_idle)
-        print >> f, print_results('Interactive time', \
-            interactive_list, result.interactive)
-        print >> f, print_results('Number of network requests', \
-            network_requests_list, result.network_requests)
-        print >> f, print_results('Total byte weight', \
-            total_byte_weight_list, result.total_byte_weight)
-        print >> f, print_results('DOM size', \
-            dom_size_list, result.dom_size)
+    result.first_contentful_paint = get_metric_results(first_contentful_paint_list, result.first_contentful_paint)
+    result.first_meaningful_paint = get_metric_results(first_meaningful_paint_list, result.first_meaningful_paint)
+    result.speed_index = get_metric_results(speed_index_list, result.speed_index)
+    result.time_to_first_byte = get_metric_results(time_to_first_byte_list, result.time_to_first_byte)
+    result.first_cpu_idle = get_metric_results(first_cpu_idle_list, result.first_cpu_idle)
+    result.interactive = get_metric_results(interactive_list, result.interactive)
+    result.network_requests = get_metric_results(network_requests_list, result.network_requests)
+    result.total_byte_weight = get_metric_results(total_byte_weight_list, result.total_byte_weight)
+    result.dom_size = get_metric_results(dom_size_list, result.dom_size)
 
     save_results(type_of_rendering)
 
