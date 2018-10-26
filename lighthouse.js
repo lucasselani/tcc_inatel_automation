bdb1020584206e36303d8c328fa1bc0fe0afe34c
Lucas Selani de Melo <lucasselani@inatel.br>
2018-10-26 10:25:42 -0300

    creating package.json and cleaning folder
    Signed-off-by: Lucas Selani de Melo <lucasselani@inatel.br>

--- lighthouse.js
+++ lighthouse.js
@@ -6,6 +6,12 @@ const opts = {
     chromeFlags: ['--show-paint-rects']
 };
 
+const CSR = 'csr'
+const SSR = 'ssr'
+const CSR_URL = 'https://client-side-app.herokuapp.com/'
+const SSR_URL = 'https://server-side-app.herokuapp.com/'
+const NUMBER_OF_INTERACTIONS = 100
+
 function launchChromeAndRunLighthouse(url, opts, config = null) {
     return chromeLauncher.launch({
         chromeFlags: opts.chromeFlags
@@ -17,6 +23,19 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
     });
 }
 
-launchChromeAndRunLighthouse('https://server-side-app.herokuapp.com/', opts, config).then(results => {
-    console.log(results)
-});
+var interactions = NUMBER_OF_INTERACTIONS
+if(process.argv.length >= 2) {
+    interactions = process.argv.slice(2);
+} 
+
+for(var i; i<interactions; i++) {
+    launchChromeAndRunLighthouse(SSR_URL, opts, config)
+        .then(results => {
+            console.log(results)
+        });
+
+    launchChromeAndRunLighthouse(CSR_URL, opts, config)
+        .then(results => {
+            console.log(results)
+        });
+}
