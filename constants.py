c67506d2e397184487ba58c3a8f157e2e0309887
Lucas Selani de Melo <lucasselani@inatel.br>
2018-10-27 09:20:28 -0300

    Updating URLs and NAMEs
    Signed-off-by: Lucas Selani de Melo <lucasselani@inatel.br>

--- constants.py
+++ constants.py
@@ -1,10 +1,18 @@
 URLS = [
-    'https://client-side-app.herokuapp.com/',
-    'https://server-side-app.herokuapp.com/'
+    'https://client-side-app.herokuapp.com/?numElements=100',
+    'https://server-side-app.herokuapp.com/?numElements=100',
+    'https://client-side-app.herokuapp.com/?numElements=1000',
+    'https://server-side-app.herokuapp.com/?numElements=1000',
+    'https://client-side-app.herokuapp.com/?numElements=10000',
+    'https://server-side-app.herokuapp.com/?numElements=10000'
 ]
 NAMES = [
-    'CSR',
-    'SSR'
+    'CSR_100',
+    'SSR_100',
+    'CSR_1000',
+    'SSR_1000',
+    'CSR_10000',
+    'SSR_10000'
 ]
 NUMBER_OF_INTERACTIONS = 100
 QUANTITY = len(URLS)
